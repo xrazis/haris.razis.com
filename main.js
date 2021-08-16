ab50a92597a9d48700a790211ec108c13c7e780e
xrazis <dev@xrazis.com>
2021-08-16 20:24:51 +0300

    Small updates

--- main.js
+++ main.js
@@ -1,22 +1,19 @@
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+const currentTheme = localStorage.getItem('xrazis-theme') || null;
 
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
-
-    if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
-    }
+    if (currentTheme === 'dark') toggleSwitch.checked = true;
 }
 
-function switchTheme(e) {
+const switchTheme = e => {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
+        localStorage.setItem('xrazis-theme', 'dark');
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light');
+        localStorage.setItem('xrazis-theme', 'light');
     }
-}
+};
 
 toggleSwitch.addEventListener('change', switchTheme, false);

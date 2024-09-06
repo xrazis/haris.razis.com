90e317fb12352b281032bc5dc74569a5b07964b4
Haris Razis <haris@razis.com>
2024-09-06 20:58:36 +0300

    Convert JPEG images to WebP format for optimization
    Replaced all JPEG images with their WebP counterparts in the balkan-tet blog post for better compression and image loading speeds. Updated the corresponding links in the markdown file to ensure the new WebP images are correctly referenced.

---
+++ scripts/imagemagick-convert-webp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Set the desired quality (adjust as needed)
+quality=80
+
+# Loop through each image in the current directory
+for file in *.jpg; do
+    # Check if the file is a regular file
+    if [ -f "$file" ]; then
+        # Get the filename without extension
+        filename="${file%.*}"
+
+        # Convert the image to WebP format with specified quality
+        convert "$file" -quality $quality "${filename}.webp"
+    fi
+done
+
+echo "Conversion complete."
